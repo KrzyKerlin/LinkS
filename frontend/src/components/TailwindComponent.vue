5be21f062718822be7f9c0263fc09b861b652537
KrzyKerlin <krzysztof.kerlin@op.pl>
2025-03-27 15:01:54 +0100

    feat(styles): setup Tailwind CSS configuration

---
+++ frontend/src/components/TailwindComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-green-500 p-4">
+        <h2 class="text-3xl text-white"> Tailwind Component</h2>
+    </div>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import '../../frontend/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
